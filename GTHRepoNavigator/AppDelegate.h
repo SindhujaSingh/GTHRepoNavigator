d749aacb15e89706458b57a3139f816c2ea048cc
SindhujaSingh <sindhujasingh@sids-laptop>
2019-10-15 11:39:19 -0700

    Initial Commit

---
+++ GTHRepoNavigator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GTHRepoNavigator
+//
+//  Created by SindhujaSingh on 10/15/19.
+//  Copyright © 2019 SindhujaSingh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GTHRepoNavigator/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  GTHRepoNavigator
+//
+//  Created by SindhujaSingh on 10/15/19.
+//  Copyright © 2019 SindhujaSingh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
