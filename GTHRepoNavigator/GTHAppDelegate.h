e9687f1d75c96d6b0d4407a26912603d268a45fe
SindhujaSingh <sindhujasingh@MacBook-Pro-3.local>
2019-10-20 22:53:39 -0700

    Change in Limit Override Macro

--- GTHRepoNavigator/GTHAppDelegate.h
+++ GTHRepoNavigator/GTHAppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define ENABLE_LIMIT_OVERRIDE 1
+#define ENABLE_LIMIT_OVERRIDE 0
 
 static NSString *const CLIENT_ID_AND_SECRET_PARAM = @"client_id=Iv1.ecccdfd29ea3958a&client_secret=68d466288e850672dd3bc70a825fc0c6c183f7b9";
 
